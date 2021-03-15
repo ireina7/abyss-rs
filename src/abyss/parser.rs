4f5d69871ffea27aa0eebee7fc0ea424ffcc7097
ireina7 <comcx.yang@gmail.com>
2021-03-15 17:00:23 +0800

    parser

--- src/abyss/parser.rs
+++ src/abyss/parser.rs
@@ -1,10 +1,58 @@
 use super::object::Object;
+use std::str::FromStr;
 
 
-pub fn parse(src: String) -> Object {
-    let _ = src;
-    Object::Nil
+#[derive(PartialEq, Debug)]
+pub struct ParseError {
+    msg: &'static str,
+    line: i32
 }
+
+impl FromStr for Object {
+    type Err = ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        parse_to_object(s)
+    }
+}
+
+fn parse_to_object(src: &str) -> Result<Object, ParseError> {
+    let _ = src.trim();
+
+    Ok(Object::Nil)
+}
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn parse_var(src: &str) -> Result<Object, ParseError> {
+    let ss = src.trim();
+    let of = |i| ss.chars().nth(i).unwrap();
+    if !(ss.len() > 0 && of(0) != '(' && of(0) != ')') {
+        return Err(ParseError { msg: "", line: 0 });
+    }
+    Ok(Object::Nil)
+}
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn parse_int(src: &str) -> Result<Object, ParseError> {
+    unimplemented!()
+}
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn parse_num(src: &str) -> Result<Object, ParseError> {
+    unimplemented!()
+}
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn parse_sym(src: &str) -> Result<Object, ParseError> {
+    unimplemented!()
+}
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn parse_str(src: &str) -> Result<Object, ParseError> {
+    unimplemented!()
+}
+
+
+
 
 
 
@@ -16,6 +64,6 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        assert_eq!(parse("".into()), Object::Nil);
+        assert_eq!("".parse(), Ok(Object::Nil));
     }
 }
