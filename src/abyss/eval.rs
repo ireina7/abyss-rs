46df54ec607a09544b5bed48233a152d0c905d14
ireina7 <comcx.yang@gmail.com>
2021-05-20 01:50:27 +0800

    many optimizations

--- src/abyss/eval.rs
+++ src/abyss/eval.rs
@@ -9,7 +9,7 @@ use std::rc::Rc;
 use super::config::HashMap;
 pub use super::object::EvalError;
 
-
+//(let ((fact (lambda (n) (case n ((0 1) (n (+ n (fact (- n 1))))))))) (fact 10000))
 /// The main Eval trait, should be able to display and debug (Clone to store in Env)
 /// 
 /// The generic type `Output` is exposed in order to evaluate abyss expression to various
@@ -23,7 +23,7 @@ impl Eval<Object> for Object {
     type Error = EvalError;
     fn eval(&self, env: &Env) -> Result<Object, EvalError> {
         let mut env = env.clone();
-        strict::evaluate(self, &mut env)
+        lazy::evaluate(self, &mut env)
     }
 }
 
@@ -394,7 +394,7 @@ pub fn evaluate(expr: &Object, env: &mut Env) -> Result<Object, EvalError> {
 
 
 
-/*
+
 
 #[allow(dead_code)]
 pub mod lazy {
@@ -468,7 +468,8 @@ fn eval_cases(expr: &Object, cases: &[Object], env: &mut Env) -> Result<Object, 
             List(xs) => match &xs[..] {
                 [pat, res] => {
                     let bind = pat.unify(&expr);
-                    if let Ok(bind) = bind {
+                    if let Ok(mut bind) = bind {
+                        let bind: HashMap<String, Rc<Object>> = bind.iter_mut().map(|(k, v)| (k.clone(), Rc::new(v.clone()))).collect();
                         env.extend(bind);
                         return evaluate(res, env)
                     } else {
@@ -513,7 +514,7 @@ fn bindings(bindings: &[Object], env: &mut Env) -> Result<(), EvalError> {
                             thunk
                         }
                     };
-                    env.insert(s.clone(), v.clone());
+                    env.insert(s.clone(), Rc::new(v.clone()));
                 }
                 _ => todo!() //unsupported yet!
             },
@@ -540,11 +541,12 @@ fn apply(f: Object, x: Object) -> Result<Object, EvalError> {
                     [ ] => evaluate(&*expr, &mut env),
                     [pat] => {
                         let bind = pat.unify(&x);
-                        if let Ok(bind) = bind {
+                        if let Ok(mut bind) = bind {
+                            let bind: HashMap<String, Rc<Object>> = bind.iter_mut().map(|(k, v)| (k.clone(), Rc::new(v.clone()))).collect();
                             if let Some(name) = name {
                                 env.insert(
                                     name.clone(), 
-                                    Closure(Some(name.clone()), Box::new(List(ps.to_vec())), expr.clone(), env.clone())
+                                    Rc::new(Closure(Some(name.clone()), Box::new(List(ps.to_vec())), expr.clone(), env.clone()))
                                 );
                             }
                             //println!("{:?}", ps);
@@ -557,11 +559,12 @@ fn apply(f: Object, x: Object) -> Result<Object, EvalError> {
                     },
                     [pat, ss @ ..] => {
                         let bind = pat.unify(&x);
-                        if let Ok(bind) = bind {
+                        if let Ok(mut bind) = bind {
+                            let bind: HashMap<String, Rc<Object>> = bind.iter_mut().map(|(k, v)| (k.clone(), Rc::new(v.clone()))).collect();
                             if let Some(name) = name.clone() {
                                 env.insert(
                                     name.clone(),
-                                    Closure(Some(name.clone()), Box::new(List(ps.to_vec())), expr.clone(), env.clone())
+                                    Rc::new(Closure(Some(name.clone()), Box::new(List(ps.to_vec())), expr.clone(), env.clone()))
                                 );
                             }
                             
@@ -638,7 +641,7 @@ fn eval_thunk(thunk: &Object) -> Result<Object, EvalError> {
             evaluate(&expr, &mut env)
         },
         Thunk(Some(name), expr, mut env) => {
-            env.insert(name.clone(), thunk.clone());
+            env.insert(name.clone(), Rc::new(thunk.clone()));
             evaluate(&expr, &mut env)
         },
         _ => Err(EvalError { msg: format!("Error while evaluating thunk: {:?}", thunk) })
@@ -654,7 +657,7 @@ fn force(thunk: &Object, env: &mut Env) -> Result<Object, EvalError> {
     match thunk_ {
         Thunk(_, _, _) => eval_thunk(thunk),
         Var(s) => {
-            let v = env.get(&s).map(|x| x.clone()).ok_or(EvalError { msg: format!("No such variable: {}", s) })?;
+            let v = env.get(&s).map(|x| (**x).clone()).ok_or(EvalError { msg: format!("No such variable: {}", s) })?;
             match &v {
                 Thunk(_, _, _) => eval_thunk(&v),
                 _ => Ok(v)
@@ -734,4 +737,3 @@ pub fn evaluate(expr: &Object, env: &mut Env) -> Result<Object, EvalError> {
 
 }//end mod lazy
 
-*/
